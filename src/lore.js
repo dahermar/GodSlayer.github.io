f2b220391e4cd0bb930eb685546de071e916ebac
pabloimbert <60478743+pabloimbert@users.noreply.github.com>
2022-05-06 21:42:04 +0200

    Update lore.js

--- src/lore.js
+++ src/lore.js
@@ -22,6 +22,7 @@
         this.sound_main_menu = this.sound.add("violin", config);
         this.Enter = this.input.keyboard.addKey('ENTER');
         this.startButton = this.add.text(0,80,'          Tras años de terror bajo la tiranía del dios Haldohr, su implacable \n          ejercito alcanza las tierras del norte.\n\n          Destruyendo todo a su paso, las tropas arrasan con una oculta \n          aldea en el bosque.\n\n          Entre los restos se vislumbra un figura. La diosa Axelia observa \n          a la joven junto a los cuerpos inertes de sus padres.\n\n          Conmovida por su dolor, le ofrece su guía en la misión de devolver\n          la paz a su reino',{fontFamily: 'GeneralFont '}).setFontSize(40);        
+        this.text = this.add.text(950, 625, 'Pulsa Enter',{fontFamily: 'GeneralFont '}).setFontSize(50);
         this.sound_main_menu.play();
     }
 
